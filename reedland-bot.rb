d2706819c92b790d4b8804e498da2c4c3ebd58b8
reedphish <reedphish@outlook.com>
2016-05-16 03:25:17 -0700

    Reedland bot

---
+++ reedland-bot.rb
@@ -0,0 +1,52 @@
+require 'rubygems'
+require 'bundler/setup'
+
+# Require as usual below
+require 'discordrb'
+require './lib/output'
+require './lib/settings'
+
+puts("Reedland-bot by Reedphish Heavy Industries. All rights reserved, 2016.")
+
+# Load settings
+begin
+	settings = Reedland::Settings.new
+	settings.load(Reedland::Settings::DEFAULT_SETTINGS_FILE)
+rescue Exception => e
+	Reedland::Output::error('Unable to load settings')#
+	exit(1)
+end
+
+# Main
+begin
+	applicationid = settings.get("discord", "applicationid")
+	token = settings.get("discord", "token")
+	serverid = settings.get("discord", "serverid")
+	channelid = settings.get("discord", "channelid")
+	botname = settings.get("discord", "botname")
+	
+	bot = Discordrb::Bot.new(token: token, application_id: applicationid, name: botname)
+	bot.message(with_text: "Hi!") do |event|
+  		event.respond "Hi, #{event.user.name}!"
+	end
+
+	bot.run(:async)
+
+=begin
+	if bot.servers.length == 0
+		puts("Please enter server invite code: ")
+		invitecode = gets.chomp
+		bot.join(invitecode)
+	end
+=end
+
+	bot.send_message(channelid, 'Bot is now active!')
+	bot.sync
+
+
+
+rescue Exception => e
+	Reedland::Output::error(e)
+	exit(1)
+end
+
